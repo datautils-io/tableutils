272aa5a17b26ef907fc33d46d4ad5182d8286d00
Vishwas Saini <vishwas.saini@fincity.com>
2024-10-28 23:02:00 +0530

    Added CellType and its different implementations

--- src/main/java/com/datautils/table/excel/cell/Cell.java
+++ src/main/java/com/datautils/table/excel/cell/Cell.java
@@ -1,13 +1,54 @@
 package com.datautils.table.excel.cell;
 
-public class Cell implements CellType<Cell>{
-	@Override
-	public Cell getDefault() {
-		return null;
+import java.util.Objects;
+
+import com.datautils.table.function.Tuple2;
+import com.datautils.table.function.Tuples;
+
+public class Cell<T extends CellType<T>> implements CellType<Cell<T>> {
+
+	private final Tuple2<Integer, Integer> position;
+
+	private final T value;
+
+	public Cell(Tuple2<Integer, Integer> position, T value) {
+		this.position = position;
+		this.value = value;
+	}
+
+	public Tuple2<Integer, Integer> position() {
+		return position;
+	}
+
+	public T value() {
+		return value;
 	}
 
 	@Override
-	public Cell copy() {
-		return null;
+	public Cell<T> getDefault() {
+		return new Cell<>(Tuples.of(0, 0), value.getDefault());
+	}
+
+	@Override
+	public Cell<T> copy() {
+		return new Cell<>(Tuples.of(position.t1(), position.t2()), value.copy());
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Cell<?> cell)) return false;
+		return Objects.equals(position, cell.position)
+				&& Objects.equals(value, cell.value);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(position, value);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Cell(pos=%s, val=%s)", position, value);
 	}
 }
--- src/main/java/com/datautils/table/excel/cell/StringCell.java
+++ src/main/java/com/datautils/table/excel/cell/StringCell.java
@@ -1,14 +1,43 @@
 package com.datautils.table.excel.cell;
 
+import java.util.Objects;
+
 public class StringCell implements CellType<StringCell> {
+
+	private final String value;
+
+	public StringCell(String value) {
+		this.value = value != null ? value : "";
+	}
+
+	public String value() {
+		return value;
+	}
 
 	@Override
 	public StringCell getDefault() {
-		return null;
+		return new StringCell("");
 	}
 
 	@Override
 	public StringCell copy() {
-		return null;
+		return new StringCell(value);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof StringCell that)) return false;
+		return Objects.equals(value, that.value);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(value);
+	}
+
+	@Override
+	public String toString() {
+		return value;
 	}
 }
